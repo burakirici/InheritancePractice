a4c10a3d0e7c2655652b3a591fb5b159b329c2a4
burak <burak@BURAK>
2024-08-06 22:10:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Ogrenci ogrenci = new()
+        {
+            Name = "Burak",
+            SurName = "Kırıcı",
+            Id = 1017
+        };
+       
+
+        Ogretmen ogretmen = new()
+        {
+            Name = "Burak",
+            SurName = "DahaKırıcı",
+            Earnings = 129
+           
+        };
+        ogretmen.WriteInfo();
+        ogrenci.WriteInfo();
+   
+    }
+}
+public abstract class BaseKisi
+{
+    public string Name { get; set; }
+    public string SurName { get; set; }
+    abstract public void WriteInfo();
+}
+public class Ogrenci : BaseKisi
+{
+    public int Id { get; set; }
+    public override void WriteInfo()
+    {
+        Console.WriteLine("Student Name: " + " " + Name + " " + "Surname: " + " " +SurName + " Student ID Number: " + " " + Id);
+    }
+
+}
+public class Ogretmen : BaseKisi
+{
+    public decimal Earnings { get; set; }
+
+    public override void WriteInfo()
+    {
+        Console.WriteLine("Salary: " + " " + Earnings + " " + "Teacher Name: " + " " + Name + " " + "Teacher Surname: " + " " + SurName);
+    }
+    
+
+}
